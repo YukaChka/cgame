5eec314084ea91a29f74d23e45b89d77b7d28903
Yuka <maxzerovol@gmail.com>
2023-09-27 23:53:04 +0300

    UP

--- 1.c
+++ 1.c
@@ -2,34 +2,24 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(void)
+
+int multiplication( int num1, int num2 ); //прототип функции
+ 
+int main()
 {
-	srand(time(NULL));
-	int gold = 15;
-	int hp_goblin= 3;
-	int rd = 80 + rand()%100;
-
-	
-	printf ("You attacked by goblin. Goblin HP: %d\n", hp_goblin);
-		
-	getchar();
-		
-	while(hp_goblin >1 )
-	{
-		hp_goblin = hp_goblin - 1;
-		printf ("You kick goblin. Goblin HP: %d\n", hp_goblin);
-		getchar();
-		
-	}
-	gold = gold +3;
-
-	printf ("You win. gold: %d\n", gold + rd);
-	printf("%d\n", 80 + rand()%100);
-	
-	
-
-
-	getchar();
-
-    return 0;
-} 
+  int num1;
+  int num2;
+ 
+  printf( "num: " );
+  scanf( "%d", &num1 );
+  scanf( "%d", &num2 );
+  printf( "result %d\n", multiplication( num1, num2 ) ); // вызов функции
+  getchar();
+  getchar();
+  return 0;
+}
+ 
+int multiplication(int num1, int num2) // определение функции
+{
+  return num1 * num2;
+}
